fc10e700487010cab9bcbe63df5b8a01c0523024
johnjianfang <john.jian.fang@gmail.com>
2013-05-25 03:01:27 -0700

    update sidedata

--- OpenSanguo.sdd/Gamedata/sidedata.lua
+++ OpenSanguo.sdd/Gamedata/sidedata.lua
@@ -1,8 +1,16 @@
 --http://springrts.com/wiki/Sidedata.lua
 local sidedata = {
 	{
-	name = "SpringPeople",
-	startUnit = "simplefactory",
-	}
+	    name = "SpringPeople",
+	    startUnit = "simplefactory",
+	},
+    {
+        name = 'Boooomer',
+        startunit = 'attackvehicledoublebarrel',
+    },
+    {
+        name = 'Erector',
+        startunit = 'buildervehicle',
+    }
 }
 return sidedata
