3edd30553fc021d65a81cbda6dd36095dffa3487
johnjianfang <john.jian.fang@gmail.com>
2013-05-25 00:58:49 -0700

    rename to opensango

---
+++ OpenSanguo.sdd/modinfo.lua
@@ -0,0 +1,15 @@
+--http://springrts.com/wiki/Modinfo.lua
+local modinfo = {
+	name = "Open Sanguo Game",
+	shortname = "STG",
+	game = "STG",
+	shortgame = "STG",
+	description = "Stragey game for Sanguo kingdoms",
+	url = "git@github.com:johnjianfang/spring.git",
+	version = "0.1",
+	modtype = 1,
+	depend = {
+		"cursors.sdz",
+	}
+}
+return modinfo
